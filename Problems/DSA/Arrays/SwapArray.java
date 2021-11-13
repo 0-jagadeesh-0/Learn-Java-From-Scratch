3e4f771d873d4cb1fe33788a7c9ff6f9e11aedd0
Devendar-Sainadh <devendar20_ug@mech.nits.ac.in>
2021-11-13 22:41:14 +0530

    adding sums

--- Problems/DSA/Arrays/SwapArray.java
+++ Problems/DSA/Arrays/SwapArray.java
@@ -2,7 +2,7 @@ public class SwapArray {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6};
         swap(arr,0,5);
-    
+
         System.out.print(arr);
     }
   static void swap(int [] arr,int index1,int index2){
--- Problems/DSA/Arrays/maxValueinArrayRange.java
+++ Problems/DSA/Arrays/maxValueinArrayRange.java
@@ -1,6 +1,7 @@
 public class maxValueinArrayRange {
         public static void main(String[] args) {
             int [] arr = {1,2,3,4,5,6};
+            
             System.out.println(maxRange(arr,1,4));
         }
         static int maxRange(int [] arr,int start, int end)
